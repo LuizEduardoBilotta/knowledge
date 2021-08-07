2f75b5ed1d89d20ce6492b8b5c0c5a3ceb12f9b7
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2021-08-07 19:28:23 -0300

    COnfigurando banco de dados e criando migrations.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const app = require('express')();
 const consign = require('consign');
+const db = require('./config/db.js');
 const port = 3000;
+
+app.db = db;
 
 consign()
   .then('./config/middlewares.js')
@@ -10,4 +13,4 @@ consign()
 
 app.listen(port, () => {
   console.log(`Backend executando na porta: [${port}]`);
-})
+});
