398a5e32e1b2ec6a2ba3ef991b07ffc5c78278ea
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2021-08-14 11:36:54 -0300

    Criando rota para consulta de usuário por ID.

--- backend/api/user.js
+++ backend/api/user.js
@@ -56,5 +56,15 @@ module.exports = app => {
       .catch(err => res.status(500).send(err))
   }
 
-  return { save, get };
+  const getById = (req, res) => {
+    id = req.params.id;
+    app.db('users')
+      .select('id', 'name', 'email', 'admin')
+      .where({ id })
+      .first()
+      .then(user => res.json(user))
+      .catch(err => res.status(500).send(err))
+  }
+
+  return { save, get, getById };
 };
